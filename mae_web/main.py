aab57cb629a79f95be4247b1e2da9b9c4e6c794b
qzkinhit <58599846+qzkinhit@users.noreply.github.com>
2022-07-31 21:29:01 +0800

    Update main.py
    决赛version

--- mae_web/main.py
+++ mae_web/main.py
@@ -28,12 +28,15 @@ def findAppendant(checkpwd,key,username,img64):
                 st.info('未知错误，请重试')
 
 if __name__ == "__main__":              
-    st.title('“见微”系统 项目演示')
-    st.subheader("基于mae认证的衍生应用系列场景：\n")
+    st.write('# “见微”系统 项目主页')
+    st.write('##### **本主页中包含系统介绍（视频演示、功能原理介绍），web、安卓衍生系统链接，web登录和注册功能**')
+    st.header(" “见微” 衍生应用系列场景：\n")
     st.markdown('&emsp;&emsp;<a href="https://mae.zkabout.xyz">[web端]基于mae认证的办公打卡系统</a>', unsafe_allow_html=True) 
     st.markdown('&emsp;&emsp;<a href="https://mae.zkabout.xyz/mae.apk">[安卓端]基于MAE的移动设备场景</a>', unsafe_allow_html=True) 
-    option = st.selectbox('功能选择',('项目介绍','登录认证', '注册'))
+    st.header("系统的主页、登录、注册功能：\n")
+    option = st.selectbox('功能选择',('项目介绍','注册','登录认证'))
     if(option=="项目介绍"):
+         #st.header(requests.url)
          st.header('“见微”——基于MAE的人脸隐私保护和双重认证系统')
          st.subheader("\n")
          video_file = open('myvideo.mp4', 'rb')
@@ -42,7 +45,7 @@ if __name__ == "__main__":
          st.subheader("\n")
          st.header("一、项目介绍")
          st.subheader("\n")
-         st.subheader("我组希望用一种新的图像语义补全模型，从数据库存储层面解决一般场景下的人脸隐私保护的问题。同时实现一种可靠的双重加密认证系统。")
+         st.write("#### 我组希望用一种新的图像语义补全模型，从数据库存储层面解决一般场景下的人脸隐私保护的问题。同时实现一种可靠的双重加密认证系统。")
          st.subheader("\n")
          st.header("二、项目流程图")
          st.subheader("\n")
@@ -50,22 +53,22 @@ if __name__ == "__main__":
          st.image(opencv_image, channels='BGR')
          st.header("三、项目优势")
          st.subheader("\n")
-         st.subheader("1.整个认证过程中，系统中不可能也没有能力还原100%的人脸信息，这保证了整个系统中无法被盗用清晰的人脸图片。\n")
-         st.subheader("2.即使原理上能复原部分人脸信息，也无法有人独立做到这一点，需要双重密钥认证，即人脸和密钥缺一不可。\n")
-         st.subheader("3.复原后的图像绝无可能用于其它系统（其它任何的人脸系统），因为不同MAE系统的密钥转mask位置的加密算法不同且不开源，保证了同一个密钥和图像块不可能用于其它系统中。\n")
-         st.subheader("4.加密过程对用户透明，用户知道服务器存储的图像块，加密过程简单。\n")
-         st.subheader("5.人脸+密钥双重认证使得准确率大大提高基本不可能识别失误。\n")
-         st.subheader("6.服务器端不接手图像原图，只接手25%的碎片化去图像化信息\n")
-         st.subheader("7.在我们设置的一些场景下可以遮挡识别和注册，戴口罩满足疫情防控要求。\n")
-         st.subheader("8.活体防伪技术和模糊检测技术，去除不合格的脸（在本系统中为了方便演示没有）\n")
+         st.write("#### 1.整个认证过程中，系统中不可能也没有能力还原100%的人脸信息，这保证了整个系统中无法被盗用清晰的人脸图片。\n")
+         st.write("#### 2.即使原理上能复原部分人脸信息，也无法有人独立做到这一点，需要双重密钥认证，即人脸和密钥缺一不可。\n")
+         st.write("#### 3.复原后的图像绝无可能用于其它系统（其它任何的人脸系统），因为不同MAE系统的密钥转mask位置的加密算法不同且不开源，保证了同一个密钥和图像块不可能用于其它系统中。\n")
+         st.write("#### 4.加密过程对用户透明，用户知道服务器存储的图像块，加密过程简单。\n")
+         st.write("#### 5.人脸+密钥双重认证使得准确率大大提高基本不可能识别失误。\n")
+         st.write("#### 6.服务器端不接手图像原图，只接手25%的碎片化去图像化信息\n")
+         st.write("#### 7.在我们设置的一些场景下可以遮挡识别和注册，戴口罩满足疫情防控要求。\n")
+         st.write("#### 8.活体防伪技术和模糊检测技术，去除不合格的脸（在本系统中为了方便演示没有）\n")
     elif(option=="注册"):
         result=None;
         st.header('注册，请上传您的人脸图片和密码')
-        st.subheader("在线拍照需要配置浏览器权限：")
-        st.subheader("1.打开谷歌浏览器后地址栏输入下面链接:chrome://flags/#unsafely-treat-insecure-origin-as-secure")
-        st.subheader("2.在打开的页面第一个输入框中输入你网页的地址，比如本页：http://mae.zkabout.xyz:8501/")
-        st.subheader("3.点击右边按钮，选择Endble")
-        st.subheader("4.点击右下角的重启按钮重启浏览器")
+        st.write("照片可以使用**图片上传**和**在线拍照**两种方式，使用**在线拍照**需要配置浏览器权限：")
+        st.write("1.打开谷歌浏览器后地址栏输入下面链接:chrome://flags/#unsafely-treat-insecure-origin-as-secure")
+        st.write("2.在打开的页面第一个输入框中输入你网页的地址，比如本页：http://mae.zkabout.xyz:8501/")
+        st.write("3.点击右边按钮，选择Endble")
+        st.write("4.点击右下角的重启按钮重启浏览器")
         genre = st.radio("选择上传图片方式",('上传图片', '在线拍照'))
         if genre == '在线拍照':
             opencv_image = st.camera_input("Take a picture")
@@ -177,17 +180,17 @@ if __name__ == "__main__":
                 st.error('设置失败，请确认您上传了人脸和密码')
 
     elif(option=="登录认证"):
-        st.header('此页面用于登录认证，确认是否注册成功')
+        # st.header('此页面用于登录认证，确认是否注册成功')
         flag1 = False
         flag2 = False
         flag4 = False  # 是否传了图片和密码
         # if flag3 is True:
         st.header('识别，请上传您的人脸并输入密码')
-        st.subheader("在线拍照需要配置浏览器权限：")
-        st.subheader("1.打开谷歌浏览器后地址栏输入下面链接:chrome://flags/#unsafely-treat-insecure-origin-as-secure")
-        st.subheader("2.在打开的页面第一个输入框中输入你网页的地址，比如本页：http://mae.zkabout.xyz:8501/")
-        st.subheader("3.点击右边按钮，选择Endble")
-        st.subheader("4.点击右下角的重启按钮重启浏览器")
+        st.write("照片可以使用**图片上传**和**在线拍照**两种方式，使用**在线拍照**需要配置浏览器权限：")
+        st.write("1.打开谷歌浏览器后地址栏输入下面链接:chrome://flags/#unsafely-treat-insecure-origin-as-secure")
+        st.write("2.在打开的页面第一个输入框中输入你网页的地址，比如本页：http://mae.zkabout.xyz:8501/")
+        st.write("3.点击右边按钮，选择Endble")
+        st.write("4.点击右下角的重启按钮重启浏览器")
         # add
         upload_face = None
         genre = st.radio("选择上传图片方式",('上传图片', '在线拍照'))
